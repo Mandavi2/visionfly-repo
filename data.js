9f3afdb22e7db8873afe326484b98fd77f1faf05
Mandavi Sahay <124423610+Mandavi2@users.noreply.github.com>
2024-08-17 17:15:25 +0530

    Add files via upload
    masonry

---
+++ data.js
@@ -0,0 +1,40 @@
+import Image_mas1 from '../../assets/Thai-plain.png'
+import Image_mas2 from '../../assets/abc.png'
+import Image_mas3 from '../../assets/kenya-mson.png'
+import Image_mas4 from '../../assets/spain-masked.png'
+import Image_mas5 from '../../assets/singa2.jpg'
+
+
+
+export const data = [
+    {
+        id: 1,
+        pic: Image_mas1,
+        travel: 'Travel to',
+        name: 'Thailand',
+    },
+    {
+        id: 2,
+        pic: Image_mas4,
+        travel: 'Travel to',
+        name: 'Spain',
+    },
+    {
+        id: 3,
+        pic: Image_mas2,
+        travel: 'Travel to',
+        name: 'Vietnam',
+    },
+    {
+        id: 4,
+        pic: Image_mas5,
+        travel: 'Travel to',
+        name: 'Singapore',
+    },
+    {
+        id: 5,
+        pic: Image_mas3,
+        travel: 'Travel to',
+        name: 'Kenya',
+    },
+]
